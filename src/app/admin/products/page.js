c262dea97429a76efe2ee82a1d0342589076a700
BereketW <bereketsodenoo@gmail.com>
2024-12-18 03:23:11 +0300

    products: added heading

--- src/app/admin/products/page.js
+++ src/app/admin/products/page.js
@@ -3,6 +3,7 @@
 import React from "react";
 import ProductsList from "@/components/admin/ProductsList";
 import { getManyProducts } from "@/data/products";
+import { bitter } from "../../../../constants";
 export const dynamic = "force-dynamic";
 export default async function page() {
   // const products = await getManyProducts();
@@ -11,6 +12,9 @@ export default async function page() {
 
   return (
     <div className="p-10">
+      <h1 className={`${bitter.className} font-medium text-4xl mb-10`}>
+        Browse our Products
+      </h1>
       <ProductsList products={allProducts} />
     </div>
   );
