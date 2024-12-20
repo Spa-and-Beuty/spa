b7214cfa5a9ae4cca4cf777499e9f88b0a31142d
BereketW <bereketsodenoo@gmail.com>
2024-12-20 18:28:48 +0300

    added animation

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { Hero } from "@/components/Hero";
 import { AboutUs } from "@/components/AboutUs";
 import { OurServices } from "@/components/OurServices";
 import { BannerScroll } from "@/components/BannerScroll";
-import { OurPlan } from "@/components/OurPlan";
+// import { OurPlan } from "@/components/OurPlan";
 import { Appointment } from "@/components/Appointment";
 import { Testimonials } from "@/components/Testimonials";
 import { Status } from "@/components/Status";
@@ -12,13 +12,14 @@ import { TryOurOil } from "@/components/TryOurOil";
 import { LatestNews } from "@/components/LatestNews";
 import { Footer } from "@/components/Footer";
 import TopBar from "@/components/TopBar";
+import OurPlan from "@/components/OurPlan";
 // import gsap from "gsap";
 // import { useGSAP } from "@gsap/react";
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
 // import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 // gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, useGSAP);
 export const dynamic = "force-dynamic";
-export default function Home() {
+export default function home() {
   return (
     <>
       <Hero />
